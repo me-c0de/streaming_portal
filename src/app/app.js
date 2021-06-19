56a57237e280ee00618e580fcb228806c37d75ab
Christopher Meyer <christopher_meyer@outlook.de>
2021-06-19 12:43:55 +0200

    Implemented Basic Functionality for Steaming Site

--- src/app/app.js
+++ src/app/app.js
@@ -1,31 +1,72 @@
-ko.components.register('movie-detail-widget', {
-    template: { require: 'movie-detail-widget/movie-detail-widget.html' },
-    viewModel: { require: 'movie-detail-widget/movie-detail-widget' }
-});
+function MovieModel(name, image, movieDetails){
+    var self = this;
+    self.name = name;
+    self.image = image;
+    self.movieDetails = movieDetails;
+    self.detailsShown = ko.observable(false)
+    self.showDetails = function() {
+        self.detailsShown() ? self.detailsShown(false) : self.detailsShown(true);
+    }
+}
 
-var newMovies = ko.observableArray([
-    { name: "Avengers Infinity War"},
-    { name: "Avengers Endgame"},
-    { name: "Spiderman: Far From Home"}
-]);
+function MovieDetailsModel(director, performer, releaseYear, description){
+    var self = this;
+    self.director = director;
+    self.performer = performer;
+    self.releaseYear = releaseYear;
+    self.description = description;
+}
 
-var popularMovies = ko.observableArray([
-    { name: "Black Panther"},
-    { name: "Doctor Strange"},
-    { name: "Antman and the Wasp"}
-]);
+function CategoryModel(category, movies){
+    var self = this;
+    self.category = category
+    self.movies = movies;
+}
 
-var watchAgain = ko.observableArray([
-    { name: "Ironman 1"},
-    { name: "Captain America: Civil War"},
-    { name: "Thor: Ragnarok"}
-]);
+function StreamingViewModel(){
+    var self = this;
+    self.categories = ko.observableArray([
+        new CategoryModel("Neue Filme", ko.observableArray([
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+        ])),
+        new CategoryModel("Beliebte Filme", ko.observableArray([
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+        ])),
+        new CategoryModel("Nochmal Ansehen", ko.observableArray([
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+            new MovieModel("Avengers: Age of Ultron", "img",
+                new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"],2012, "Description")),
+        ]))
+    ])
+};
 
-ko.applyBindings(newMovies);
-ko.applyBindings(popularMovies);
-ko.applyBindings(watchAgain);
 
-
+ko.applyBindings(new StreamingViewModel());
 
 
 
