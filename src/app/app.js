ae544561729c2d6c7692e0b0abd67145f1635200
einchieved <49752360+einchieved@users.noreply.github.com>
2021-06-25 15:31:39 +0200

    fixed slider

--- src/app/app.js
+++ src/app/app.js
@@ -24,9 +24,10 @@ function MovieDetailsModel(director, performer, releaseYear, description) {
     });
 }
 
-function CategoryModel(category, movies) {
+function CategoryModel(category, index, movies) {
     var self = this;
     self.category = category
+    self.index = index;
     self.movies = movies;
 }
 
@@ -34,7 +35,7 @@ function StreamingViewModel() {
     var self = this;
     self.chosenFilm = ko.observable();
     self.categories = ko.observableArray([
-        new CategoryModel("Neue Filme", ko.observableArray([
+        new CategoryModel("Neue Filme", 5, ko.observableArray([
             new MovieModel("Avengers: Age of Ultron", "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/B6D17EE56247DB49AC11AC031EB2FB43189D230EF3592C7C4808F9B0EB363FCD/scale?width=1200&aspectRatio=1.78&format=jpeg",
                 new MovieDetailsModel("Joss Whedon", ["Robert Downey Jr.", "Chris Evans", "Marc Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Jeremy Renner"], 2015, "Gute Absichten richten verheerenden Schaden an, als Tony Stark unwissentlich Ultron, ein K.I.-Monster schafft, das durch Massenvernichtung den Weltfrieden erreichen will.")),
 
@@ -53,7 +54,7 @@ function StreamingViewModel() {
             new MovieModel("Captain Marvel", "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/980264E5D398E019B2DBD30C1100DAD0C1393EDD795482086D4E83239F91194B/scale?width=1200&aspectRatio=1.78&format=jpeg",
                 new MovieDetailsModel("Anna Boden,  Ryan Fleck", ["Brie Larson", "Samuel L. Jackson", "Ben Mendelsohn", "Djimon Hounsou", "Lee Pace", "Lashana Lynch"], 2019, "Marvel Studios Captain Marvel ist ein Abenteuer aus einer bisher nicht gesehenen Ära in der Geschichte des Marvel Cinematic Universe. Es verfolgt die Reise von Carol Danvers (Brie Larson), die den jungen Agenten Nick Fury (Samuel L. Jackson) kennenlernt und schließlich zu einer der mächtigsten Heldinnen des Universums wird.")),
         ])),
-        new CategoryModel("Beliebte Filme", ko.observableArray([
+        new CategoryModel("Beliebte Filme", 4, ko.observableArray([
             new MovieModel("Rogue One: A Star Wars Story", "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F5D8C3E9CC39E61A6CA9DD0B60945F3CF72280352BA814A525B863A47B937E88/scale?width=1200&aspectRatio=1.78&format=jpeg",
                 new MovieDetailsModel("Gareth Edwards", ["Felicity Jones", "Diego Luna", "Ben Mendelsohn", "Donnie Yen", "Mads Mikkelsen", "Alan Tudyk"], 2016, "Lucasfilm bringt uns mit Rogue One: A Star Wars Story ein episches Abenteuer. In einer Zeit des Konflikts schließt sich eine Gruppe von ungewöhnlichen Helden zusammen, um die Pläne für den Todesstern, die ultimative Waffe des Imperiums, zu stehlen. Dieses Schlüsselereignis in der Star-Wars-Timeline bringt ganz normale Menschen zusammen, die Teil von etwas viel Größerem werden. Einige Sequenzen enthalten Blitzlicht-Effekte, die sich auf lichtempfindliche Zuschauer auswirken können.")),
 
@@ -72,7 +73,7 @@ function StreamingViewModel() {
             new MovieModel("Toy Story", "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A1B6A3B47FBB4CCA1AEC201B730EDBA5491E404C37C32419C055002AFA38ABB9/scale?width=1200&aspectRatio=1.78&format=jpeg",
                 new MovieDetailsModel("John Lasseter", ["Tom Hanks", "Tim Allen", "Don Rickles", "Jim Varney", "Wallace Shawn", "John Ratzenberger"], 1995, "Willkommen in einer Welt, in der Spielsachen spielen, während ihre Besitzer weg sind. Erlebe gemeinsam mit Woody, Buzz und ihren Freunden ein Abenteuer voller Humor, Herz und Freundschaft.")),
         ])),
-        new CategoryModel("Nochmal Ansehen", ko.observableArray([
+        new CategoryModel("Nochmal Ansehen", 3, ko.observableArray([
             new MovieModel("Avengers: Age of Ultron", "https://a-static.besthdwallpaper.com/avengers-endgame-tapete-3840x2160-39967_54.jpg",
                 new MovieDetailsModel("Joss Whedon", ["RDJ", "Chris Evans", "Marc Ruffalo"], 2012, "Description")),
             new MovieModel("Avengers: Age of Ultron", "https://a-static.besthdwallpaper.com/avengers-endgame-tapete-3840x2160-39967_54.jpg",
